21224dd59c1fb48d428a032c2d52daf6c4dc5ba1
Ramil Latypov <r.latypov@mediasoft.team>
2018-10-03 13:00:56 +0400

    changes

--- spider/spider/spiders/check_categories_komus.py
+++ spider/spider/spiders/check_categories_komus.py
@@ -1,5 +1,6 @@
 import scrapy
 from selenium import webdriver
+from urllib.parse import urljoin
 from spider.settings import (
                             SELECTOR_BUTTON_MORE_KOMUS,
                             SELECTOR_LIST_GROUPS_KOMUS,
@@ -12,7 +13,7 @@ from spider.settings import (
                             SELECTOR_PRODUCT_LIST_KOMUS
                         )
 
-from urllib.parse import urljoin
+
 from spider.items import (
                             ItemProduct
                         )
@@ -22,8 +23,8 @@ class KomusSpider(scrapy.Spider):
     name = "komus_spider"
     allowed_domains = ['komus.ru']
     start_urls = [
-                    'https://www.komus.ru/search?text=стол',
-                    # 'https://www.komus.ru/search?text=ножницы',
+                    # 'https://www.komus.ru/search?text=стол',
+                    'https://www.komus.ru/search?text=ножницы',
                     # 'https://www.komus.ru/search?text=скрепки',
                     # 'https://www.komus.ru/search?text=ручка синяя'
                     # 'https://www.komus.ru/search?text=стол стекляный',
@@ -40,47 +41,34 @@ class KomusSpider(scrapy.Spider):
         self.driver = webdriver.Firefox(executable_path=PATH_TO_DRIVER)
 
     def parse(self, response):
-        self.driver.get(response.url)
 
-        index = 0
-        while self.is_checks:
-            # слева панель с чекбоксами найденных групп
+        if response.url not in self.visited_urls:
+            self.driver.get(response.url)
+            self.visited_urls.append(response.url)
+            index = 0
             groups_check = self.driver.find_elements_by_xpath(SELECTOR_LIST_GROUPS_KOMUS)
-            # кнопка "Показать еще" если есть в панели найденных групп
             button_more = self.driver.find_elements_by_xpath(SELECTOR_BUTTON_MORE_KOMUS)
-            # если есть панель с категориями
             groups_list = self.driver.find_elements_by_xpath(SELECTOR_PANEL_GROUPS_KOMUS)
-            # если есть панель с категориями
             tag_list = self.driver.find_elements_by_xpath(SELECTOR_TAGS_KOMUS)
 
             if groups_check:
                 if button_more:
                     button_more[0].click()
                 groups_check[index].click()
-                index += 1
-                if index == 3:
-                    self.is_checks = False
-                    break
-            # elif groups_list:
-            #     self.logger.warning(groups_list)
-            #     break
-            # elif tag_list:
-            #     self.logger.warning(tag_list)
-            #     break
-            # else:
-            #     break
-            #
-        # проходим по товарам на одной странице
-        for link in response.xpath(SELECTOR_PRODUCT_LIST_KOMUS):
-            url = urljoin(response.url, link.extract())
-            # парсим товар
-            yield response.follow(url, callback=self.parse_product)
 
-        next_pages = self.driver.find_elements_by_xpath(SELECTOR_PRODUCT_NEXT_PAGES_KOMUS)
-        next_page = next_pages[-1]
-        next_page.click()
-        yield response.follow(self.driver.current_url, callback=self.parse)
-        # self.driver.close()
+            # проходим по товарам на одной странице
+            for link in response.xpath(SELECTOR_PRODUCT_LIST_KOMUS):
+                url = urljoin(response.url, link.extract())
+                # парсим товар
+                yield response.follow(url, callback=self.parse_product)
+
+            next_pages = self.driver.find_elements_by_xpath(SELECTOR_PRODUCT_NEXT_PAGES_KOMUS)
+
+            if next_pages:
+                next_page = next_pages[-1]
+                next_page.click()
+                yield response.follow(self.driver.current_url, callback=self.parse)
+            # self.driver.close()
 
     def parse_product(self, response):
         item = ItemProduct()
